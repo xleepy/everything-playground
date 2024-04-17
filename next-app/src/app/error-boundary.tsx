db03f5f2b858cf17bd164c97b79b0b1f7a71c638
Radmir Gurkov <ragux0@gmail.com>
2024-04-18 00:45:46 +0200

    jotai and use playground

---
+++ next-app/src/app/error-boundary.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, Component } from "react";
+
+type Props = {
+  fallback: ReactNode;
+  children: ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error) {
+    // Example "componentStack":
+    //   in ComponentThatThrows (created by App)
+    //   in ErrorBoundary (created by App)
+    //   in div (created by App)
+    //   in App
+    console.error(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
